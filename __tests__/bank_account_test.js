bb758e302bb64aaf454097aa9b2576e5b75bc13f
mirandaweston <mweston105@yahoo.co.uk>
2023-03-21 20:26:00 +0000

    Updating BankAccount test suite: tests given acceptance criteria

--- __tests__/bank_account_test.js
+++ __tests__/bank_account_test.js
@@ -137,4 +137,45 @@ const BankAccount = require('../src/bank_account.js');
       dateSpy.mockRestore();
     });
   });
- });
+
+  describe('multiple operations', () => {
+    it('handles two deposits and one withdrawal before updating the transactions array', () => {
+      const account = new BankAccount();
+      const dateOne = new Date('2023-01-10T00:00:00Z');
+      const dateTwo = new Date('2023-01-13T00:00:00Z');
+      const dateThree = new Date('2023-01-14T00:00:00Z');
+
+      const dateSpy = jest.spyOn(global, 'Date').mockImplementation(() => dateOne);
+      account.deposit(1000);
+
+      dateSpy.mockImplementation(() => dateTwo);
+      account.deposit(2000);
+
+      dateSpy.mockImplementation(() => dateThree);
+      account.withdraw(500);
+
+      expect(account.transactions).toEqual([
+        {
+          date: dateOne,
+          credit: 1000,
+          debit: null,
+          balance: 1000,
+        },
+        {
+          date: dateTwo,
+          credit: 2000,
+          debit: null,
+          balance: 3000,
+        },
+        {
+          date: dateThree,
+          credit: null,
+          debit: 500,
+          balance: 2500,
+        },
+      ]);
+
+      dateSpy.mockRestore();
+    });  
+  })
+});
